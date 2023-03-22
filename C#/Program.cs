f0b1364a236fe3c674606aabecd660c9328c5d89
Leon <yaroshenko.leonid@gmail.com>
2023-03-22 09:55:41 +0300

    Solution added

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArray()
+{
+    Console.Write("Enter size of array ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i} element of array ");
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        Console.Write(array[i] + " ");
+}
+
+string[] array = CreateStringArray();
+int amount = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+        amount++;
+
+string[] result = new string[amount];
+amount = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+    {
+        result[amount] = array[i];
+        amount++;
+    }
+
+PrintArray(array);
+Console.Write("-> ");
+PrintArray(result);
